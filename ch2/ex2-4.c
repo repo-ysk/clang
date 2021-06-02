115d0f411d5f4a8ef997b1a4c01749a9b453cf66
skyun <skyun@yonsei.ac.kr>
2021-06-02 11:04:06 +0900

    ch2 v1

--- ch2/ex2-4.c
+++ ch2/ex2-4.c
@@ -7,8 +7,8 @@ int main(void)
 	printf("Enter the radius of a circle : ");
 	scanf("%f",&r);
 	s = 3.14159 * r * r;
-	printf("Area is %f\n",s);			/* 변수 S 값 출력 */
-	printf("Diameter is %f\n",2*r);		/* 수식 2*r 값 출력 */
+	printf("Area is %f\n",s);		// 변수 S 값 출력 
+	printf("Diameter is %f\n",2*r);		// 수식 2*r 값 출력 
 	
 	return 0;
 }	
--- ch2/ex2-8.c
+++ ch2/ex2-8.c
@@ -5,8 +5,8 @@
 int main()
 {
 	printf("INT_MAX = %d\n", INT_MAX);			// int 최대값
-	printf("FLT_MAX_10_EXP = %d\n", FLT_MAX_10_EXP);		// float 최대지수(10진수)
-	printf("FLT_MIN_10_EXP = %d\n", FLT_MIN_10_EXP);		// float 최소지수(10진수)
+	printf("FLT_MAX_10_EXP = %d\n", FLT_MAX_10_EXP);	// float 최대지수(10진수)
+	printf("FLT_MIN_10_EXP = %d\n", FLT_MIN_10_EXP);	// float 최소지수(10진수)
 	printf("FLT_DIG = %d\n", FLT_DIG);			// float 유효자리수(10진수)
 
 	return 0;
